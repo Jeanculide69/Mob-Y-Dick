257ae837a51ac4b19b0aac49dcd4dbd3957a105f
Jeanculide69 <cartiercorentin@gmail.com>
2026-05-17 22:31:45 +0200

    feat: configure active google adsense publisher client id to pub-6244816354585064

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ const GoogleAd = ({ slot = '1234567890', format = 'auto', style = { display: 'bl
       <ins 
         className="adsbygoogle"
         style={style}
-        data-ad-client="ca-pub-6316279933560759"
+        data-ad-client="ca-pub-6244816354585064"
         data-ad-slot={slot}
         data-ad-format={format}
         data-full-width-responsive="true"
